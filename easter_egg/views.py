9fe569f2ba7a8b362d19a602e937ec14f90318e7
Jason Ward <jason.ward@policystat.com>
2014-07-20 17:09:07 -0400

    refs #5: The index page now does what it should be doing.

--- easter_egg/views.py
+++ easter_egg/views.py
@@ -1,5 +1,26 @@
+import json
+from tempfile import NamedTemporaryFile
+
+import requests
+
 from django.http import HttpResponse
+
+from easter_egg.models import split_image
 
 
 def index(request):
-    return HttpResponse('Test')
+    content = requests.get(
+        'https://www.google.com/images/srpr/logo11w.png',
+    ).content
+    with NamedTemporaryFile('rw+b') as f:
+        f.write(content)
+        f.seek(0)
+        f.flush()
+        data = [
+            split_image(f.name, i, 10)
+            for i in range(1, 11)
+        ]
+    return HttpResponse(
+        json.dumps(data),
+        content_type='application/json',
+    )
