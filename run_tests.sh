a748235c042800680614d525de0ca67bd04a08ef
Jason Ward <jason.ward@policystat.com>
2014-07-25 18:10:28 -0400

    refs #6: && in the test script to get testing to work correctly.

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-coverage run --source='easter_egg/' manage.py test --verbosity=2; coverage report --show-missing --fail-under=100 --omit="*test*.py" && find easter_egg -name '*.py' | xargs flake8
+coverage run --source='easter_egg/' manage.py test --verbosity=2 && coverage report --show-missing --fail-under=100 --omit="*test*.py" && find easter_egg -name '*.py' | xargs flake8
